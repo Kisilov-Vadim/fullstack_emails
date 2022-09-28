59d7462dd440e62e6109c6917b8a8bd315a52b59
Vadim Kisilov <49349216+Kisilov-Vadim@users.noreply.github.com>
2022-09-28 17:30:50 +0200

    feat: add stripe keys

--- server/src/config/prod.ts
+++ server/src/config/prod.ts
@@ -1,6 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID as string,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
   mongoURI: process.env.MONGO_URI as string,
   cookieKey: process.env.COOKIE_KEY as string,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY as string,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY as string,
 };
