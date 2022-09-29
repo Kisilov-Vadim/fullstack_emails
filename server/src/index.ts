0fab5c37a38596c28fbc6c6aca8b20a7b4ed4468
Vadim Kisilov <49349216+Kisilov-Vadim@users.noreply.github.com>
2022-09-29 10:58:26 +0200

    debug: add console.log

--- server/src/index.ts
+++ server/src/index.ts
@@ -32,12 +32,10 @@ billingRoutes(app);
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
 
-  console.log('__dirname', __dirname);
-
   app.use(express.static('../../client/build'));
   app.get('*', (req, res) => {
     res.sendFile(
-      path.resolve(__dirname, '../../', 'client', 'build', 'index.html')
+      path.resolve(__dirname, '../../client', 'build', 'index.html')
     );
   });
 }
